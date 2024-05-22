a0a5354a825b4f19f517ff3d21c682ac7249f413
Mateo <mateobonavia@gmail.com>
2024-05-22 17:14:14 -0300

    logic to wishlist ready

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,8 +1,36 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
 import { Heart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
+  const { user } = useUser();
+
+  const [loading, setLoading] = useState(false);
+  const [signedInUser, setSignedInUser] = useState<UserType | null>(null);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const getUser = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch("/api/users");
+      const data = await res.json();
+      setSignedInUser(data);
+      setIsLiked(data.wishlist.includes(product._id));
+      setLoading(false);
+    } catch (error) {
+      console.log("[users_GET]", error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      getUser();
+    }
+  }, []);
+
   return (
     <Link
       href={`/products/${product._id}`}
@@ -23,7 +51,7 @@ const ProductCard = ({ product }: { product: ProductType }) => {
         <div className="flex justify-between items-center">
           <p className="text-body-bold">${product.price}</p>
           <button>
-            <Heart />
+            <Heart fill={`${isLiked ? "red" : "white"}`} />
           </button>
         </div>
       </div>
