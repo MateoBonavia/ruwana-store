06f7a848a0181428b98b9bb1b143508e8031b048
Mateo <mateobonavia@gmail.com>
2024-05-23 12:35:40 -0300

    ready images in detail

--- app/(root)/products/[productId]/page.tsx
+++ app/(root)/products/[productId]/page.tsx
@@ -1,5 +1,19 @@
-const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+import Gallery from "@/components/Gallery";
+import { getProductDetails } from "@/lib/actions";
+
+const ProductDetails = async ({
+  params,
+}: {
+  params: { productId: string };
+}) => {
+  const productDetail = await getProductDetails(params.productId);
+  console.log(productDetail);
+
+  return (
+    <div className="flex justify-center items-start gap-16 py-10 px-5 max-md:flex-col max-md:items-center">
+      <Gallery productMedia={productDetail.media} />
+    </div>
+  );
 };
 
 export default ProductDetails;
