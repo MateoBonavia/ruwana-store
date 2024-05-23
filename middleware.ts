7beb6ba961fa00f5c606dca3ca9ed3a3d192d9f1
Mateo <mateobonavia@gmail.com>
2024-05-23 11:57:12 -0300

    fixed middleware to enter the product detail without a session

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/((?!$|_next|favicon.ico).*)"]);
-
-// const isProtectedRoute = createRouteMatcher([
-//   "/((?!api/products).*)",
-//   "/((?!api/collections).*)",
-//   "/(.*)",
-// ]);
+const isProtectedRoute = createRouteMatcher([
+  "/cart(/.*)?", // Protege el carrito y todas las subrutas del carrito
+  "/api/trpc(/.*)?", // Protege las rutas de API que quieras proteger
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
@@ -15,3 +12,9 @@ export default clerkMiddleware((auth, req) => {
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+// const isProtectedRoute = createRouteMatcher([
+//   "/((?!api/products).*)",
+//   "/((?!api/collections).*)",
+//   "/(.*)",
+// ]);
