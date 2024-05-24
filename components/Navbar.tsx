65ee21695626a0c6cb13750afd804e0165db93d2
Mateo <mateobonavia@gmail.com>
2024-05-24 11:23:30 -0300

    ready logic add product to cart

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,11 @@ import Link from "next/link";
 import Logo from "../public/Logo.png";
 import { UserButton, useUser } from "@clerk/nextjs";
 import { useState } from "react";
+import useCart from "@/lib/hooks/useCart";
 
 const Navbar = () => {
   const { user } = useUser();
+  const cart = useCart();
 
   const [dropdownMenu, setDropdownMenu] = useState(false);
 
@@ -28,7 +30,7 @@ const Navbar = () => {
           className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white"
         >
           <ShoppingCart />
-          <p className="text-base-bold">Carrito (0)</p>
+          <p className="text-base-bold">Carrito ({cart.cartItems.length})</p>
         </Link>
 
         {user && (
