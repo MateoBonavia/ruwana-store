771796aeeefd3bb9c8b319fc6f162c823b50ccaf
Mateo <mateobonavia@gmail.com>
2024-06-24 12:48:36 -0300

    ready suggestion when type in input | a little detail the suggestino whos two times | Missing save that information and send in metadata when create order on mercadopago

--- components/ShippingForm.tsx
+++ components/ShippingForm.tsx
@@ -1,13 +1,10 @@
 "use client";
-import { useCallback, useEffect, useState } from "react";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import { useEffect, useRef, useState } from "react";
+import { Popover, PopoverContent } from "@/components/ui/popover";
 import { Textarea } from "./ui/textarea";
 import { Input } from "./ui/input";
 import { useForm, Controller } from "react-hook-form";
+import useDebounce from "@/lib/hooks/useDebounce";
 
 type GeoapifySuggestion = {
   properties: {
@@ -16,43 +13,50 @@ type GeoapifySuggestion = {
   };
 };
 
-type GeoapifyResponse = {
-  features: GeoapifySuggestion[];
-};
-
 const ShippingForm = () => {
   const { control, handleSubmit, setValue: setFormValue } = useForm();
   const [suggestions, setSuggestions] = useState<GeoapifySuggestion[]>([]);
   const [open, setOpen] = useState(false);
   const [inputValue, setInputValue] = useState("");
+  const debouncedInputValue = useDebounce(inputValue, 750);
+  const skipNextInputChange = useRef(false);
 
-  const handleInputChange = async (value: string) => {
-    setInputValue(value);
-    setOpen(true);
-    const apiKey = process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY;
-    console.log(value);
-    if (value.length > 2) {
-      try {
-        const response = await fetch(
-          `https://api.geoapify.com/v1/geocode/autocomplete?text=${value}&apiKey=${apiKey}`
-        );
-        const data: GeoapifyResponse = await response.json();
-        console.log("Data received from Geoapify:", data);
-        setSuggestions(data.features ?? []);
-      } catch (error) {
-        console.error("Error fetching data from Geoapify:", error);
-        setSuggestions([]);
-      }
-    } else {
-      setSuggestions([]);
+  const handleInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (skipNextInputChange.current) {
+      skipNextInputChange.current = false;
+      return;
     }
+
+    const value = e.target.value;
+    setInputValue(value);
+    setOpen(value.length > 2);
   };
 
   useEffect(() => {
-    if (inputValue.length > 2) {
-      handleInputChange(inputValue);
-    }
-  }, [inputValue]);
+    const fetchSuggestions = async () => {
+      if (debouncedInputValue.length > 2) {
+        try {
+          const apiKey = process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY;
+          const res = await fetch(
+            `https://api.geoapify.com/v1/geocode/autocomplete?text=${debouncedInputValue}&apiKey=${apiKey}`
+          );
+          const data = await res.json();
+          console.log(data);
+          setSuggestions(data.features ?? []);
+          setOpen(data.features.length > 0);
+        } catch (error) {
+          console.error("Error fetching data from Geoapify:", error);
+          setSuggestions([]);
+          setOpen(false);
+        }
+      } else {
+        setSuggestions([]);
+        setOpen(false);
+      }
+    };
+
+    fetchSuggestions();
+  }, [debouncedInputValue]);
 
   const onSubmit = (data: any) => {
     console.log(data);
@@ -60,48 +64,44 @@ const ShippingForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-2">
+      <div className="mb-2 relative">
         <label>Provincia, ciudad y dirección</label>
         <Controller
           name="fullAddress"
           control={control}
           render={({ field }) => (
-            <div>
-              <Popover open={open} onOpenChange={setOpen}>
-                <Input
-                  {...field}
-                  onChange={(e) => {
-                    handleInputChange(e.target.value);
-                    // const value = e.target.value;
-                    // setInputValue(value);
-                    // setOpen(true);
-                  }}
-                  onFocus={() => setOpen(true)}
-                  onBlur={() => {
-                    // Delay closing the popover to allow click events to be processed
-                    setTimeout(() => setOpen(false), 200);
-                  }}
-                />
-                <PopoverContent>
-                  {suggestions.map((suggestion) => (
-                    <div
-                      key={suggestion.properties.place_id}
-                      onClick={() => {
-                        setInputValue(suggestion.properties.formatted);
-                        setFormValue(
-                          "fullAddress",
-                          suggestion.properties.formatted
-                        );
-                        setSuggestions([]);
-                        setOpen(false);
-                      }}
-                      className="cursor-pointer"
-                    >
-                      {suggestion.properties.formatted}
-                    </div>
-                  ))}
-                </PopoverContent>
-              </Popover>
+            <div className="relative">
+              <Input
+                {...field}
+                onChange={handleInputChange}
+                value={inputValue}
+              />
+              {open && (
+                <div className="absolute z-10 bg-white border border-gray-300 mt-1 w-full rounded-md shadow-lg">
+                  {suggestions.length > 0 ? (
+                    suggestions.map((suggestion) => (
+                      <div
+                        key={suggestion.properties.place_id}
+                        onClick={() => {
+                          skipNextInputChange.current = true;
+                          setInputValue(suggestion.properties.formatted);
+                          setFormValue(
+                            "fullAddress",
+                            suggestion.properties.formatted
+                          );
+                          setSuggestions([]);
+                          setOpen(false);
+                        }}
+                        className="cursor-pointer p-2 hover:bg-gray-200"
+                      >
+                        {suggestion.properties.formatted}
+                      </div>
+                    ))
+                  ) : (
+                    <div className="p-2">No suggestions found</div>
+                  )}
+                </div>
+              )}
               <div>
                 <label>Comentarios adicionales</label>
                 <Textarea />
